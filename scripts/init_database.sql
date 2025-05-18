096b512034049708b2013252aa250fedfbd7078d
kandarineha09 <kandarineha09@gmail.com>
2025-05-19 01:55:20 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -10,9 +10,7 @@ CREATE DATABASE DataWarehouse;
 -- Switch to the new database
 USE DataWarehouse;
 
--- Create schemas (simulated using prefixes in MySQL, since MySQL does not support schemas like MSSQL)
--- In MySQL, the typical approach is to create separate databases or use table name prefixes instead.
--- But starting MySQL 5.7+, you can mimic schemas using database-like structures.
+
 
 -- For educational or organizational purposes, we can simulate schemas like:
 CREATE TABLE bronze_example (
