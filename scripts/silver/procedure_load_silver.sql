941b114d12fcbaf34048568be49308b75973f6ab
kandarineha09 <kandarineha09@gmail.com>
2025-05-19 06:49:04 +0530

    Update procedure_load_silver.sql

--- scripts/silver/procedure_load_silver.sql
+++ scripts/silver/procedure_load_silver.sql
@@ -63,44 +63,65 @@ BEGIN
     SET end_time = NOW();
     SELECT 'crm_prd_info duration', TIMESTAMPDIFF(SECOND, start_time, end_time);
 
+    
     -- crm_sales_details
-    SET start_time = NOW();
-    TRUNCATE TABLE silver_crm_sales_details;
-    INSERT INTO silver_crm_sales_details (
-        sls_ord_num, sls_prd_key, sls_cust_id,
-        sls_order_dt, sls_ship_dt, sls_due_dt,
-        sls_sales, sls_quantity, sls_price
-    )
-    SELECT
-        sls_ord_num,
-        sls_prd_key,
-        sls_cust_id,
-        CASE 
-            WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt) != 8 THEN NULL
-            ELSE STR_TO_DATE(sls_order_dt, '%Y%m%d')
-        END as sls_order_dt,
-        CASE 
-            WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt) != 8 THEN NULL
-            ELSE STR_TO_DATE(sls_ship_dt, '%Y%m%d')
-        END as sls_ship_dt,
-        CASE 
-            WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt) != 8 THEN NULL
-            ELSE STR_TO_DATE(sls_due_dt, '%Y%m%d')
-        END as sls_due_dt,
-        CASE 
-            WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
-                THEN sls_quantity * ABS(sls_price)
-            ELSE sls_sales
-        END as sls_sales,
-        sls_quantity,
-        CASE 
-            WHEN sls_price IS NULL OR sls_price <= 0
-                THEN sls_sales / NULLIF(sls_quantity, 0)
-            ELSE sls_price
-        END as sls_price
-    FROM datawarehouse.bronze_crm_sales_details;
-    SET end_time = NOW();
-    SELECT 'crm_sales_details duration', TIMESTAMPDIFF(SECOND, start_time, end_time);
+SET start_time = NOW();
+TRUNCATE TABLE silver_crm_sales_details;
+INSERT INTO silver_crm_sales_details (
+    sls_ord_num, sls_prd_key, sls_cust_id,
+    sls_order_dt, sls_ship_dt, sls_due_dt,
+    sls_sales, sls_quantity, sls_price
+)
+SELECT
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    -- Fixing sls_order_dt
+    CASE 
+        WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt) != 8 THEN STR_TO_DATE('2000-01-01', '%Y-%m-%d')
+        ELSE STR_TO_DATE(sls_order_dt, '%Y%m%d')
+    END as sls_order_dt,
+    
+    -- Fixing sls_ship_dt
+    CASE 
+        WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt) != 8 THEN
+            DATE_ADD(
+                CASE 
+                    WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt) != 8 THEN STR_TO_DATE('2000-01-01', '%Y-%m-%d')
+                    ELSE STR_TO_DATE(sls_order_dt, '%Y%m%d')
+                END, INTERVAL 2 DAY)
+        ELSE STR_TO_DATE(sls_ship_dt, '%Y%m%d')
+    END as sls_ship_dt,
+
+    -- Fixing sls_due_dt
+    CASE 
+        WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt) != 8 THEN
+            DATE_ADD(
+                CASE 
+                    WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt) != 8 THEN STR_TO_DATE('2000-01-01', '%Y-%m-%d')
+                    ELSE STR_TO_DATE(sls_order_dt, '%Y%m%d')
+                END, INTERVAL 7 DAY)
+        ELSE STR_TO_DATE(sls_due_dt, '%Y%m%d')
+    END as sls_due_dt,
+
+    -- Fix sales if broken
+    CASE 
+        WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+            THEN sls_quantity * ABS(sls_price)
+        ELSE sls_sales
+    END as sls_sales,
+
+    sls_quantity,
+
+    -- Fix price if broken
+    CASE 
+        WHEN sls_price IS NULL OR sls_price <= 0
+            THEN sls_sales / NULLIF(sls_quantity, 0)
+        ELSE sls_price
+    END as sls_price
+FROM datawarehouse.bronze_crm_sales_details;
+SET end_time = NOW();
+SELECT 'crm_sales_details duration', TIMESTAMPDIFF(SECOND, start_time, end_time);
 
     -- erp_cust_az12
     SET start_time = NOW();
